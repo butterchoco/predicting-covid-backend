6f2ba5a5e7e37524fda3f21b83fea99031e7013c
razaqa <dhafins18@gmail.com>
2020-12-21 10:28:54 +0700

    Add dot product matrix multiplication function

--- Function/Matrix.hs
+++ Function/Matrix.hs
@@ -14,3 +14,21 @@ col n = map (head . drop n)
 
 element :: Int -> Int -> [[a]] -> a
 element r c x = (head . col c) [(row r x)]
+
+row_len_min1 :: [a] -> Int
+row_len_min1 [] = -1
+row_len_min1 x = length x - 1
+
+col_len_min1 :: [[a]] -> Int
+col_len_min1 [] = -1
+col_len_min1 x = (length . transpose) x - 1
+
+calculate :: (t1 -> t2 -> a) -> [t1] -> [t2] -> [a]
+calculate f [] [] = []
+calculate f [] _ = error "Both arrays' length should be equal"
+calculate f _ [] = error "Both arrays' length should be equal"
+calculate f (x:xs) (y:ys) = (f x y) : (calculate f xs ys)
+
+dot :: Num a => [[a]] -> [[a]] -> [[a]]
+dot x y = [[ sum (mult_row_col rx cy) | rx <- [0..row_len_min1 x]] | cy <- [0..col_len_min1 y] ] where
+    mult_row_col rx cy = calculate (*) (row rx x) (col cy y)
