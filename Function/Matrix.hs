c87ed0e1228a168739e39a3d6d9fe7a15432ca18
razaqa <dhafins18@gmail.com>
2020-12-21 10:26:16 +0700

    Add function to get specific row, column, and element in matrix

--- Function/Matrix.hs
+++ Function/Matrix.hs
@@ -3,3 +3,14 @@ module Function.Matrix () where
 transpose :: [[a]] -> [[a]]
 transpose ([]:_) = []
 transpose x = (map head x) : transpose (map tail x)
+
+row :: Int -> [[a]] -> [a]
+row _ [] = error "Matrix size (row) should be > n"
+row n (x:xs) | n <= 0 = x
+             | otherwise = row (n-1) xs
+
+col :: Int -> [[a]] -> [a]
+col n = map (head . drop n)
+
+element :: Int -> Int -> [[a]] -> a
+element r c x = (head . col c) [(row r x)]
