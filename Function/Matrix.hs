d989ce9fdd1374844101c47ff22429514f7753a5
razaqa <dhafins18@gmail.com>
2020-12-22 21:44:21 +0700

    Refactor minor, calculate, and dot

--- Function/Matrix.hs
+++ Function/Matrix.hs
@@ -22,17 +22,18 @@ col_len_min1 :: [[a]] -> Int
 col_len_min1 [] = -1
 col_len_min1 x = (length . transpose) x - 1
 
-calculate :: (t1 -> t2 -> a) -> [t1] -> [t2] -> [a]
-calculate f [] [] = []
-calculate f (x:xs) (y:ys) = (f x y) : (calculate f xs ys)
+join :: (t1 -> t2 -> a) -> [t1] -> [t2] -> [a]
+join f [] [] = []
+join f (x:xs) (y:ys) = (f x y) : (join f xs ys)
 
 dot :: Num a => [[a]] -> [[a]] -> [[a]]
-dot x y = [[ sum (mult_row_col rx cy) | rx <- [0..row_len_min1 x]] | cy <- [0..col_len_min1 y] ] where
-    mult_row_col rx cy = calculate (*) (row rx x) (col cy y)
+dot x y = [[ sum (mult_row_col rx cy) | rx <- [0..row_len_min1 x]] | cy <- [0..col_len_min1 y]] where
+    mult_row_col rx cy = join (*) (row rx x) (col cy y)
+
+minor :: Eq a => Int -> Int -> [[a]] -> [[a]]
+minor _ _ [] = []
+minor r c x = remove_empty [[ element ir ic x | ic <- [0..col_len_min1 x], r /= ir, c /= ic] | ir <- [0..row_len_min1 x]]
 
 remove_empty :: Eq a => [[a]] -> [[a]]
 remove_empty x = filter (\a -> a /= []) x
 
-minor :: Eq a => Int -> Int -> [[a]] -> [[a]]
-minor _ _ [] = []
-minor r c x = remove_empty [[ element ir ic x | ir <- [0..row_len_min1 x], r /= ir, c /= ic] | ic <- [0..col_len_min1 x] ]
